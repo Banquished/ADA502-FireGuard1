bb092958bf034a754f5ca7f4b457c43b3dd3062c
Ole <105752308+Banquished@users.noreply.github.com>
2024-02-20 21:04:54 +0100

    added base html files for landing, about & contact pages

---
+++ Frontend-app/API/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.template import loader
+
+# Create your views here.
+def index(request):
+    context = {}
+    return render(request, 'API/index.html', context)
+
+def about(request):
+    context = {}
+    return render(request, 'API/about.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'API/contact.html', context)
+
