a89bb2a58e62925318e202fe790ccef9aee4f4c8
Ole <105752308+Banquished@users.noreply.github.com>
2024-03-20 16:03:24 +0100

    Finished React App V1.0

--- Frontend-app/API/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class React(models.Model):
-    employee = models.CharField(max_length=100)
-    department = models.CharField(max_length=200)
